b28b23fc520ec46aea54e16079e19c7ae45ddd51
Diego Ramírez Estrada <diego.ramirez.estrada@gmail.com>
2018-10-08 20:16:43 -0700

    ok

--- app/src/main/java/com/saptra/sieron/myapplication/Controller/ReadBarCodeActivity.java
+++ app/src/main/java/com/saptra/sieron/myapplication/Controller/ReadBarCodeActivity.java
@@ -1,7 +1,6 @@
 package com.saptra.sieron.myapplication.Controller;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -12,8 +11,6 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.CameraSource;
